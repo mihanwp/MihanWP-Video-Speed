86d4b4fde6a5061aee4d24e2ba711628e5ef6927
Reza HoseiniRad <admin@mihanwp.com>
2021-09-07 14:56:12 +0430

    css fix

--- mihanwp-video-speed.php
+++ mihanwp-video-speed.php
@@ -4,7 +4,7 @@
   Plugin URI:  https://mihanwp.com/
   Author:      MihanWP
   Author URI:  https://mihanwp.com
-  Version:     1.0
+  Version:     1.1
   Description: WordPress plugin for change video speed
   Text Domain: wp-video-speed
   Domain Path: /languages
@@ -13,7 +13,7 @@
 defined('ABSPATH') or die;
 
 function mihanwp_video_speed_load_assets(){
-    $version = 1.0;
+    $version = 1.1;
     wp_enqueue_script('mwspeedjs', plugin_dir_url(__FILE__). 'app.js' , ['jquery'], $version, true);
     wp_localize_script('mwspeedjs', 'mwspeed', ['mwspeedimg' => plugin_dir_url(__FILE__)]);
     wp_enqueue_style( 'mwspeedcss', plugin_dir_url(__FILE__) . 'app.css',[],$version);
